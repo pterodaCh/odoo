613e80f9619547f4fd1ac5ee83880fcfac14e686
vlad <vlad.os.progr@gmail.com>
2024-08-01 16:46:33 +0300

    Added report template

--- custom_addons/iot_certification/__manifest__.py
+++ custom_addons/iot_certification/__manifest__.py
@@ -17,7 +17,8 @@
         'views/iot_certification_menu.xml',
         'views/iot_certification_testing_laboratory.xml',
         'views/iot_certification_additional_order_view.xml',
-        'views/iot_certification_enterprise.xml'
+        'views/iot_certification_enterprise.xml',
+        'reports/iot_certification_report.xml'
     ],
     'installable': True,
     'auto_install': True,
