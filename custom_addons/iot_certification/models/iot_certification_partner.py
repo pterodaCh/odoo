c5e8616aa19a053fa60a7d3013f1b6112172192a
anastasia <vdovina.nastya03@gmail.com>
2024-10-21 19:39:13 +0300

    bank field

--- custom_addons/iot_certification/models/iot_certification_partner.py
+++ custom_addons/iot_certification/models/iot_certification_partner.py
@@ -16,7 +16,12 @@ class PartnerExtension(models.Model):
         'partner_id', 
         string='Підприємства'
     )
+
+    duplicated_bank_account_partners_count = fields.Integer(string='Duplicated Bank Account Partner Count', compute='_compute_duplicated_bank_account_partners_count')
     
     @api.model
     def _default_view(self):
         return self.env.ref('view_res_partner_form_extension')
+
+    def _compute_duplicated_bank_account_partners_count(self):
+        self.duplicated_bank_account_partners_count = 0
